9cd7aae8651176bf329438724e2407bd562db3b4
Abanoub Amin <abanoub.amin.fcis@gmail.com>
2022-11-18 16:19:20 +0200

    chore: add static list for super heroes

---
+++ src/constants/Heroes.js
@@ -0,0 +1,52 @@
+export const featuredCharacters = [
+  {
+    name: 'AVENGERS',
+    imageURL:
+      'https://i.annihil.us/u/prod/marvel/i/mg/3/a0/537ba3793915b/standard_xlarge.jpg',
+  },
+  {
+    name: 'SPIDER-MAN',
+    imageURL:
+      'https://i.annihil.us/u/prod/marvel/i/mg/9/30/538cd33e15ab7/standard_xlarge.jpg',
+  },
+  {
+    name: 'IRON MAN',
+    imageURL:
+      'https://i.annihil.us/u/prod/marvel/i/mg/6/a0/55b6a25e654e6/standard_xlarge.jpg',
+  },
+  {
+    name: 'BLACK PANTHER',
+    imageURL:
+      'https://i.annihil.us/u/prod/marvel/i/mg/1/c0/537ba2bfd6bab/standard_xlarge.jpg',
+  },
+  {
+    name: 'DEADPOOL',
+    imageURL:
+      'https://i.annihil.us/u/prod/marvel/i/mg/5/c0/537ba730e05e0/standard_xlarge.jpg',
+  },
+  {
+    name: 'CAPTAIN AMERICA',
+    imageURL:
+      'https://i.annihil.us/u/prod/marvel/i/mg/3/50/537ba56d31087/standard_xlarge.jpg',
+  },
+  {
+    name: 'ANT-MAN',
+    imageURL:
+      'https://i.annihil.us/u/prod/marvel/i/mg/6/90/54ad7297b0a59/standard_xlarge.jpg',
+  },
+  {
+    name: 'CAPTAIN MARVEL',
+    imageURL:
+      'https://i.annihil.us/u/prod/marvel/i/mg/c/10/537ba5ff07aa4/standard_xlarge.jpg',
+  },
+  {
+    name: 'GUARDIANS OF THE GALAXY',
+    imageURL:
+      'https://i.annihil.us/u/prod/marvel/i/mg/c/10/537babe1c1f66/standard_xlarge.jpg',
+  },
+  {
+    name: 'WOLVERINE',
+    imageURL:
+      'https://i.annihil.us/u/prod/marvel/i/mg/2/60/537bcaef0f6cf/standard_xlarge.jpg',
+  },
+];
